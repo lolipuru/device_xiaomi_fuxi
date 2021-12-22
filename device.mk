7c7683676cd610379a9cd5c8be09b487d561689a
Sasha Smundak <asmundak@google.com>
2021-12-22 15:26:50 -0800

    fuxi: Use soong_config_ macros to set Soong config variables.
    Bug: 198503843
    Test: treehugger
    Change-Id: If913e8165d8e1a51386c82cc57190890093ab3f2

--- device.mk
+++ device.mk
@@ -53,10 +53,7 @@ PRODUCT_PACKAGES += \
     android.hardware.atrace@1.0-service
 
 # Audio
-SOONG_CONFIG_NAMESPACES += android_hardware_audio
-SOONG_CONFIG_android_hardware_audio += \
-    run_64bit
-SOONG_CONFIG_android_hardware_audio_run_64bit := true
+$(call soong_config_set, android_hardware_audio, run_64bit, true)
 
 PRODUCT_PACKAGES += \
     android.hardware.audio@7.1-impl \
