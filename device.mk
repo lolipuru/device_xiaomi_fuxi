2f835f23e73101b1d297f7bc7588429d224b48df
Arian <arian.kulmer@web.de>
2022-06-30 17:41:25 +0200

    fuxi: Build qti aidl power hal
    Change-Id: I7c9352e26653c51401c6825ca8988e7cb05479bc

--- device.mk
+++ device.mk
@@ -126,6 +126,13 @@ PRODUCT_PACKAGES += \
 # Properties
 include $(LOCAL_PATH)/properties/default.mk
 
+# Power
+PRODUCT_PACKAGES += \
+    android.hardware.power-service-qti
+
+PRODUCT_COPY_FILES += \
+    vendor/qcom/opensource/power/config/taro/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+
 # QTI
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml \
