0f1aa4562b48e956a99cf0f6a9c4d59eab9bee73
Arian <arian.kulmer@web.de>
2023-03-06 17:19:06 +0100

    fuxi: Initial sepolicy and enforcing selinux
    Change-Id: Ia21793576649e8518e79e4680e0b79b6a9331720

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -183,6 +183,7 @@ BOARD_BUILD_SYSTEM_ROOT_IMAGE := false
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Verified Boot
