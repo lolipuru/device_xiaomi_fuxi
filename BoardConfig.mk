4aa8e47e9ae70cacf01ca2b44a3533265a8cddd7
lolipuru <banstellazz@gmail.com>
2023-08-01 20:36:45 +0700

    fuxi: sync props from 14.0.7.0.TMCMIXM
    Signed-off-by: lolipuru <banstellazz@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -153,9 +153,9 @@ TARGET_POWERHAL_MODE_EXT := $(DEVICE_PATH)/power/power-mode.cpp
 
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/properties/odm.prop
-#TARGET_PRODUCT_PROP += $(DEVICE_PATH)/properties/product.prop
-#TARGET_SYSTEM_PROP += $(DEVICE_PATH)/properties/system.prop
-#TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/properties/system_ext.prop
+TARGET_PRODUCT_PROP += $(DEVICE_PATH)/properties/product.prop
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/properties/system.prop
+TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/properties/system_ext.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/properties/vendor.prop
 
 # Recovery
