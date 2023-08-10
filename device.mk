30d11a7e308469e4b17ad84d75c762c5454df931
lolipuru <banstellazz@gmail.com>
2023-08-10 20:43:07 +0700

    fuxi: build vendor.qti.hardware.btconfigstore@1.0.vendor
    Signed-off-by: lolipuru <banstellazz@gmail.com>

--- device.mk
+++ device.mk
@@ -105,6 +105,7 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.1.vendor \
     android.hardware.bluetooth.audio@2.0-impl \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
+    vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
 PRODUCT_COPY_FILES += \
