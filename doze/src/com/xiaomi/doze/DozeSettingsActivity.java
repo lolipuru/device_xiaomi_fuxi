66e8ebc25bd3d6d50e443d03aa71e8ef382d9126
Bruno Martins <bgcngm@gmail.com>
2023-08-26 15:53:04 +0200

    fuxi: Fix build after qpr1 merge

--- doze/src/com/xiaomi/doze/DozeSettingsActivity.java
+++ doze/src/com/xiaomi/doze/DozeSettingsActivity.java
@@ -21,7 +21,7 @@ package com.xiaomi.doze;
 import android.os.Bundle;
 
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
-import com.android.settingslib.collapsingtoolbar.R;
+import com.android.settingslib.widget.R;
 
 public class DozeSettingsActivity extends CollapsingToolbarBaseActivity {
 
