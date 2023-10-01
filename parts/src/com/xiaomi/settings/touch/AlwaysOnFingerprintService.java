8d1094bd8530de5490d08ef119b06559789349a9
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2023-10-01 14:56:48 +0200

    fuxi: Always set alwaysonudfps on screen off
     * In some cases, such as when face unlock is used, UDFPS would be disabled upon successful authentication. In this case, Always On UDFPS and Tap to Wake wouldn't work anymore.
    
    Change-Id: Ie709363d0af15f0c579a08268b248560a48d81c6

--- parts/src/com/xiaomi/settings/touch/AlwaysOnFingerprintService.java
+++ parts/src/com/xiaomi/settings/touch/AlwaysOnFingerprintService.java
@@ -7,9 +7,11 @@
 package com.xiaomi.settings.touch;
 
 import android.app.Service;
+import android.content.BroadcastReceiver;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.database.ContentObserver;
 import android.net.Uri;
 import android.os.Handler;
@@ -28,9 +30,12 @@ public class AlwaysOnFingerprintService extends Service {
     private static final String SECURE_KEY_TAP = "doze_tap_gesture";
     private static final String SECURE_KEY_UDFPS = "screen_off_udfps_enabled";
 
+    private static boolean mIsAofEnabled = false;
+
     private Context mContext;
     private Handler mHandler;
     private ContentResolver mContentResolver;
+    private CustomBroadcastReceiver mCustomBroadcastReceiver;
     private CustomSettingsObserver mCustomSettingsObserver;
 
     @Override
@@ -38,12 +43,14 @@ public class AlwaysOnFingerprintService extends Service {
         if (DEBUG) Log.d(TAG, "Creating service");
         mContentResolver = getContentResolver();
         mHandler = new Handler(Looper.getMainLooper());
+        mCustomBroadcastReceiver = new CustomBroadcastReceiver();
         mCustomSettingsObserver = new CustomSettingsObserver(mHandler);
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startid) {
         if (DEBUG) Log.d(TAG, "Starting service");
+        mCustomBroadcastReceiver.register();
         mCustomSettingsObserver.register();
         mCustomSettingsObserver.update();
         return START_STICKY;
@@ -58,6 +65,34 @@ public class AlwaysOnFingerprintService extends Service {
     @Override
     public IBinder onBind(Intent intent) {
         return null;
+    }
+
+    private class CustomBroadcastReceiver extends BroadcastReceiver {
+        CustomBroadcastReceiver() {
+            super();
+        }
+
+        public void register() {
+            if (DEBUG) Log.d(TAG, "CustomBroadcastReceiver: register");
+            registerReceiver(mCustomBroadcastReceiver, new IntentFilter(Intent.ACTION_SCREEN_ON));
+            registerReceiver(mCustomBroadcastReceiver, new IntentFilter(Intent.ACTION_SCREEN_OFF));
+        }
+
+        void update(boolean enable) {
+            if (DEBUG) Log.d(TAG, "CustomSettingsObserver: update");
+            SystemProperties.set("persist.sys.fuxi.alwaysonudfps", enable ? "1" : "0");
+        }
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equals(Intent.ACTION_SCREEN_ON)) {
+                if (DEBUG) Log.d(TAG, "CustomBroadcastReceiver: ACTION_SCREEN_ON");
+                update(false);
+            } else if (intent.getAction().equals(Intent.ACTION_SCREEN_OFF)) {
+                if (DEBUG) Log.d(TAG, "CustomBroadcastReceiver: ACTION_SCREEN_OFF");
+                update(mIsAofEnabled);
+            }
+        }
     }
 
     private class CustomSettingsObserver extends ContentObserver {
@@ -76,7 +111,7 @@ public class AlwaysOnFingerprintService extends Service {
             boolean st2w = Secure.getInt(mContentResolver, SECURE_KEY_TAP, 0) != 0;
             boolean udfps = Secure.getInt(mContentResolver, SECURE_KEY_UDFPS, 0) != 0;
             if (DEBUG) Log.d(TAG, "CustomSettingsObserver: SECURE_KEY_TAP: " + st2w + ", SECURE_KEY_UDFPS: " + udfps);
-            SystemProperties.set("persist.sys.fuxi.alwaysonudfps", (st2w || udfps) ? "1" : "0");
+            mIsAofEnabled = st2w || udfps;
         }
 
         @Override
