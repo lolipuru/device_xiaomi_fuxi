b3ee5db157a2fed12f61949a26ac57729ecce29c
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2023-10-03 10:19:14 +0200

    fuxi: Rework AlwaysOnFingerprintService
    Change-Id: I3ffc8321312eb6596f9070c90cb4b6717380936b

--- parts/src/com/xiaomi/settings/touch/AlwaysOnFingerprintService.java
+++ parts/src/com/xiaomi/settings/touch/AlwaysOnFingerprintService.java
@@ -30,29 +30,28 @@ public class AlwaysOnFingerprintService extends Service {
     private static final String SECURE_KEY_TAP = "doze_tap_gesture";
     private static final String SECURE_KEY_UDFPS = "screen_off_udfps_enabled";
 
-    private static boolean mIsAofEnabled = false;
+    private boolean mIsAofEnabled;
 
-    private Context mContext;
-    private Handler mHandler;
     private ContentResolver mContentResolver;
-    private CustomBroadcastReceiver mCustomBroadcastReceiver;
-    private CustomSettingsObserver mCustomSettingsObserver;
+    private ScreenStateReceiver mScreenStateReceiver;
+    private SettingsObserver mSettingsObserver;
+    private final Handler mHandler = new Handler();
 
     @Override
     public void onCreate() {
+        super.onCreate();
         if (DEBUG) Log.d(TAG, "Creating service");
         mContentResolver = getContentResolver();
-        mHandler = new Handler(Looper.getMainLooper());
-        mCustomBroadcastReceiver = new CustomBroadcastReceiver();
-        mCustomSettingsObserver = new CustomSettingsObserver(mHandler);
+        mScreenStateReceiver = new ScreenStateReceiver();
+        mSettingsObserver = new SettingsObserver(mHandler);
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startid) {
         if (DEBUG) Log.d(TAG, "Starting service");
-        mCustomBroadcastReceiver.register();
-        mCustomSettingsObserver.register();
-        mCustomSettingsObserver.update();
+        mScreenStateReceiver.register();
+        mSettingsObserver.register();
+        mSettingsObserver.update();
         return START_STICKY;
     }
 
@@ -67,56 +66,49 @@ public class AlwaysOnFingerprintService extends Service {
         return null;
     }
 
-    private class CustomBroadcastReceiver extends BroadcastReceiver {
-        CustomBroadcastReceiver() {
-            super();
-        }
-
+    private final class ScreenStateReceiver extends BroadcastReceiver {
         public void register() {
-            if (DEBUG) Log.d(TAG, "CustomBroadcastReceiver: register");
-            registerReceiver(mCustomBroadcastReceiver, new IntentFilter(Intent.ACTION_SCREEN_ON));
-            registerReceiver(mCustomBroadcastReceiver, new IntentFilter(Intent.ACTION_SCREEN_OFF));
-        }
-
-        void update(boolean enable) {
-            if (DEBUG) Log.d(TAG, "CustomSettingsObserver: update");
-            SystemProperties.set("persist.sys.fuxi.alwaysonudfps", enable ? "1" : "0");
+            if (DEBUG) Log.d(TAG, "ScreenStateReceiver: register");
+            registerReceiver(mScreenStateReceiver, new IntentFilter(Intent.ACTION_SCREEN_ON));
+            registerReceiver(mScreenStateReceiver, new IntentFilter(Intent.ACTION_SCREEN_OFF));
         }
 
         @Override
         public void onReceive(Context context, Intent intent) {
-            if (intent.getAction().equals(Intent.ACTION_SCREEN_ON)) {
-                if (DEBUG) Log.d(TAG, "CustomBroadcastReceiver: ACTION_SCREEN_ON");
-                update(false);
-            } else if (intent.getAction().equals(Intent.ACTION_SCREEN_OFF)) {
-                if (DEBUG) Log.d(TAG, "CustomBroadcastReceiver: ACTION_SCREEN_OFF");
-                update(mIsAofEnabled);
+            switch (intent.getAction()) {
+                case Intent.ACTION_SCREEN_ON:
+                    if (DEBUG) Log.d(TAG, "Received ACTION_SCREEN_ON");
+                    SystemProperties.set("persist.sys.fuxi.alwaysonudfps", "0");
+                    break;
+                case Intent.ACTION_SCREEN_OFF:
+                    if (DEBUG) Log.d(TAG, "Received ACTION_SCREEN_OFF");
+                    SystemProperties.set("persist.sys.fuxi.alwaysonudfps", mIsAofEnabled ? "1" : "0");
+                    break;
             }
         }
     }
 
-    private class CustomSettingsObserver extends ContentObserver {
-        CustomSettingsObserver(Handler handler) {
+    private final class SettingsObserver extends ContentObserver {
+        SettingsObserver(Handler handler) {
             super(handler);
         }
 
         public void register() {
-            if (DEBUG) Log.d(TAG, "CustomSettingsObserver: register");
+            if (DEBUG) Log.d(TAG, "SettingsObserver: register");
             mContentResolver.registerContentObserver(Secure.getUriFor(SECURE_KEY_TAP), false, this);
             mContentResolver.registerContentObserver(Secure.getUriFor(SECURE_KEY_UDFPS), false, this);
         }
 
         void update() {
-            if (DEBUG) Log.d(TAG, "CustomSettingsObserver: update");
             boolean st2w = Secure.getInt(mContentResolver, SECURE_KEY_TAP, 0) != 0;
             boolean udfps = Secure.getInt(mContentResolver, SECURE_KEY_UDFPS, 0) != 0;
-            if (DEBUG) Log.d(TAG, "CustomSettingsObserver: SECURE_KEY_TAP: " + st2w + ", SECURE_KEY_UDFPS: " + udfps);
+            if (DEBUG) Log.d(TAG, "SettingsObserver: SECURE_KEY_TAP: " + st2w + ", SECURE_KEY_UDFPS: " + udfps);
             mIsAofEnabled = st2w || udfps;
         }
 
         @Override
         public void onChange(boolean selfChange, Uri uri) {
-            if (DEBUG) Log.d(TAG, "CustomSettingsObserver: onChange: " + uri.toString());
+            if (DEBUG) Log.d(TAG, "SettingsObserver: onChange: " + uri.toString());
             if (uri.equals(Secure.getUriFor(SECURE_KEY_TAP))
                     || uri.equals(Secure.getUriFor(SECURE_KEY_UDFPS))) {
                 update();
