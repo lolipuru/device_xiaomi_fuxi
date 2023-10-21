5b9196128edec5abda9323723b915de2dac9f4a1
LuK1337 <priv.luk@gmail.com>
2023-10-21 16:34:40 +0200

    fuxi: Build android.frameworks.sensorservice@1.0.vendor
    Required by mm-pp-deps.
    
    Change-Id: Iaf821dcbb07d7820f2fc3ff5ca9cca5e973360f2

--- device.mk
+++ device.mk
@@ -440,7 +440,7 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.frameworks.sensorservice@1.0 \
+    android.frameworks.sensorservice@1.0.vendor \
     android.hardware.sensors-service.multihal \
     libsensorndkbridge \
     sensors.xiaomi
