aef9bd4e35866015f99ec69d298647aa0a82102e
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-11-23 21:55:37 +0530

    fuxi: Set standard display color mode in AOD
    Other modes have a slight glowing effect as the black pixels do not
    completely turn off, which is not ideal for AOD as it can increase
    power consumption.
    
    Change-Id: I5670ad813047b2a70184af307f9b07382763cdae
    
    fuxi: Use correct API to check AOD status
    
    Change-Id: I5ef7ae158546e3edbce6f8144cfe401e5fb0d087

--- parts/src/com/xiaomi/settings/display/ColorModeService.java
+++ parts/src/com/xiaomi/settings/display/ColorModeService.java
@@ -10,9 +10,12 @@ import static android.provider.Settings.System.DISPLAY_COLOR_MODE;
 import static com.xiaomi.settings.display.DfWrapper.DfParams;
 
 import android.app.Service;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.database.ContentObserver;
+import android.hardware.display.AmbientDisplayConfiguration;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.SystemProperties;
@@ -22,6 +25,8 @@ import android.util.Log;
 
 import java.util.Map;
 
+import com.xiaomi.settings.display.DfWrapper.DfParams;
+
 public class ColorModeService extends Service {
     private static final String TAG = "XiaomiPartsColorModeService";
     private static final boolean DEBUG = true;
@@ -29,11 +34,13 @@ public class ColorModeService extends Service {
     private static final int DEFAULT_COLOR_MODE = SystemProperties.getInt(
             "persist.sys.sf.native_mode", 0);
 
+    private static final DfParams STANDARD_PARAMS = new DfParams(2, 2, 255);
+
     /* color mode -> displayfeature (mode, value, cookie) */
     private static final Map<Integer, DfParams> COLOR_MAP = Map.of(
         258 /* vivid */, new DfParams(0, 2, 255),
         256 /* saturated */, new DfParams(1, 2, 255),
-        257 /* standard */, new DfParams(2, 2, 255),
+        257 /* standard */, STANDARD_PARAMS,
         269 /* original */, new DfParams(26, 1, 0),
         268 /* p3 */, new DfParams(26, 2, 0),
         267 /* srgb */, new DfParams(26, 3, 0)
@@ -42,7 +49,11 @@ public class ColorModeService extends Service {
     private static final int EXPERT_MODE = 26;
     private static final DfParams EXPERT_PARAMS = new DfParams(26, 0, 10);
 
-    private final ContentObserver mSettingObserver = new ContentObserver(new Handler()) {
+    private Handler mHandler = new Handler();
+    private AmbientDisplayConfiguration mAmbientConfig;
+    private boolean mIsDozing;
+
+    private final ContentObserver mSettingObserver = new ContentObserver(mHandler) {
         @Override
         public void onChange(boolean selfChange) {
             Log.e(TAG, "SettingObserver: onChange");
@@ -50,12 +61,46 @@ public class ColorModeService extends Service {
         }
     };
 
+    private final BroadcastReceiver mScreenStateReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.e(TAG, "onReceive: " + intent.getAction());
+            switch (intent.getAction()) {
+                case Intent.ACTION_SCREEN_ON:
+                    if (mIsDozing) {
+                        mIsDozing = false;
+                        mHandler.removeCallbacksAndMessages(null);
+                        mHandler.postDelayed(() -> {
+                            Log.e(TAG, "Was in AOD, restore color mode");
+                            setCurrentColorMode();
+                        }, 100);
+                    }
+                    break;
+                case Intent.ACTION_SCREEN_OFF:
+                    if (!mAmbientConfig.alwaysOnEnabled(UserHandle.USER_CURRENT)) {
+                        Log.e(TAG, "AOD is not enabled");
+                        mIsDozing = false;
+                        break;
+                    }
+                    mIsDozing = true;
+                    mHandler.removeCallbacksAndMessages(null);
+                    Log.e(TAG, "Entered AOD, set color mode to standard");
+                    DfWrapper.setDisplayFeature(STANDARD_PARAMS);
+                    break;
+            }
+        }
+    };
+
     @Override
     public void onCreate() {
         super.onCreate();
         Log.e(TAG, "onCreate");
+        mAmbientConfig = new AmbientDisplayConfiguration(this);
         getContentResolver().registerContentObserver(Settings.System.getUriFor(DISPLAY_COLOR_MODE),
                     false, mSettingObserver, UserHandle.USER_CURRENT);
+        IntentFilter screenStateFilter = new IntentFilter(Intent.ACTION_SCREEN_ON);
+        screenStateFilter.addAction(Intent.ACTION_SCREEN_OFF);
+        registerReceiver(mScreenStateReceiver, screenStateFilter);
         setCurrentColorMode();
     }
 
@@ -69,6 +114,7 @@ public class ColorModeService extends Service {
     public void onDestroy() {
         Log.e(TAG, "onDestroy");
         getContentResolver().unregisterContentObserver(mSettingObserver);
+        unregisterReceiver(mScreenStateReceiver);
         super.onDestroy();
     }
 
@@ -78,6 +124,10 @@ public class ColorModeService extends Service {
     }
 
     private void setCurrentColorMode() {
+        if (mIsDozing) {
+            Log.e(TAG, "setCurrentColorMode: skip in AOD");
+            return;
+        }
         final int colorMode = Settings.System.getIntForUser(getContentResolver(),
                 DISPLAY_COLOR_MODE, DEFAULT_COLOR_MODE, UserHandle.USER_CURRENT);
         if (!COLOR_MAP.containsKey(colorMode)) {
