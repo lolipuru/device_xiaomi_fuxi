79bfa4b5b72e67302dd40e438ee4a9f53a01f725
ghostrider-reborn <gh0strider.2k18.reborn@gmail.com>
2024-02-09 12:20:04 +0530

    fuxi: Switch to 64-bit only builds
    Co-authored-by: saikiran2001 <bjsaikiran@gmail.com>

--- lineage_fuxi.mk
+++ lineage_fuxi.mk
@@ -5,7 +5,7 @@
 #
 
 # Configure core_64_bit.mk
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit common LineageOS configurations
