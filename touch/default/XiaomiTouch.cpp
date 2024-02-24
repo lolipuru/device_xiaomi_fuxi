xiaomi-8550/android_device_xiaomi_xiaomi13

fuxi: Add custom xiaomi touch AIDL HAL
Co-authored-by: Harshit Jain <reach@harsh1998.dev>
Change-Id: I9eecd3e14cf60c9fed921d1c695d9079956982f4

touch/default/XiaomiTouch.cpp
@@ -1,0 +1,60 @@
+//
+// Copyright (C) 2023 Paranoid Android
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#define TAG "XiaomiTouchHAL"
+
+#include "XiaomiTouch.h"
+
+#include <android/binder_manager.h>
+#include <android-base/logging.h>
+#include <android-base/unique_fd.h>
+
+#include <algorithm>
+#include <cassert>
+#include <cerrno>
+#include <chrono>
+
+#include <fcntl.h>
+#include <signal.h>
+#include <sys/ioctl.h>
+
+// XiaomiTouch device declarations
+#define SET_CUR_VALUE 0
+#define TOUCH_DEV_PATH "/dev/xiaomi-touch"
+#define TOUCH_ID 0
+#define TOUCH_MAGIC 'T'
+#define TOUCH_IOC_SET_CUR_VALUE _IO(TOUCH_MAGIC, SET_CUR_VALUE)
+
+using file_fd = android::base::unique_fd;
+
+namespace aidl::vendor::aospa::xiaomitouch {
+
+::ndk::ScopedAStatus XiaomiTouch::setModeValue(int32_t mode, int32_t value) {
+    if (!DISABLE_DEBUG) LOG(INFO) << TAG << ": " << "setModeValue called with value = " << value;
+
+    file_fd fd(open(TOUCH_DEV_PATH, O_RDWR));
+
+    int buf[3] = {TOUCH_ID, mode, value};
+
+    if (fd.get() == -1) {
+        if (!DISABLE_DEBUG) LOG(ERROR) << TAG << ": " << "Failed to open: " << TOUCH_DEV_PATH;
+    } else {
+        if (ioctl(fd.get(), TOUCH_IOC_SET_CUR_VALUE, &buf) == -1) {
+            if (!DISABLE_DEBUG) LOG(ERROR) << TAG << ": " << "Failed to write to: " << TOUCH_DEV_PATH;
+        } else {
+            if (!DISABLE_DEBUG) LOG(INFO) << TAG << ": " << "Wrote touch mode value as: " << value
+                << " for mode: " << mode;
+        }
+    }
+
+    return ::ndk::ScopedAStatus::ok();
+}
+
+XiaomiTouch::XiaomiTouch(void) {
+    if (!DISABLE_DEBUG) LOG(INFO) << TAG << ": " << "XiaomiTouch: Constructor";
+}
+
+}

touch/default/XiaomiTouch.h
@@ -1,0 +1,22 @@
+//
+// Copyright (C) 2023 Paranoid Android
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#pragma once
+
+#include <aidl/vendor/aospa/xiaomitouch/BnXiaomiTouch.h>
+
+#include <mutex>
+#include <thread>
+
+namespace aidl::vendor::aospa::xiaomitouch {
+
+class XiaomiTouch : public BnXiaomiTouch {
+    public:
+    XiaomiTouch(void);
+    ::ndk::ScopedAStatus setModeValue(int mode, int value) override;
+};
+
+}

touch/default/service.cpp
@@ -1,0 +1,33 @@
+//
+// Copyright (C) 2023 Paranoid Android
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include <android-base/logging.h>
+#include <android/binder_manager.h>
+#include <android/binder_process.h>
+#include <binder/ProcessState.h>
+#include <binder/IServiceManager.h>
+
+#include "XiaomiTouch.h"
+
+using ::aidl::vendor::aospa::xiaomitouch::XiaomiTouch;
+
+template <class C>
+static void registerAsService(std::shared_ptr<C> service, const char *inst) {
+  const std::string instance = std::string() + C::descriptor + "/" + inst;
+  binder_status_t status =
+      AServiceManager_addService(service->asBinder().get(), instance.c_str());
+  CHECK(status == STATUS_OK);
+  if (!DISABLE_DEBUG) LOG(INFO) << "Register done for instance " << inst;
+}
+
+int main() {
+  ABinderProcess_setThreadPoolMaxThreadCount(8);
+  if (!DISABLE_DEBUG) LOG(INFO) << "Starting XiaomiTouch service";
+  registerAsService(ndk::SharedRefBase::make<XiaomiTouch>(), "default");
+
+  ABinderProcess_joinThreadPool();
+  return -1; // should never get here
+}

lolipuru/device_xiaomi_fuxi

touch/default/XiaomiTouch.cpp
@@ -1,0 +1,60 @@
+//
+// Copyright (C) 2023 Paranoid Android
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#define TAG "XiaomiTouchHAL"
+
+#include "XiaomiTouch.h"
+
+#include <android/binder_manager.h>
+#include <android-base/logging.h>
+#include <android-base/unique_fd.h>
+
+#include <algorithm>
+#include <cassert>
+#include <cerrno>
+#include <chrono>
+
+#include <fcntl.h>
+#include <signal.h>
+#include <sys/ioctl.h>
+
+// XiaomiTouch device declarations
+#define SET_CUR_VALUE 0
+#define TOUCH_DEV_PATH "/dev/xiaomi-touch"
+#define TOUCH_ID 0
+#define TOUCH_MAGIC 'T'
+#define TOUCH_IOC_SET_CUR_VALUE _IO(TOUCH_MAGIC, SET_CUR_VALUE)
+
+using file_fd = android::base::unique_fd;
+
+namespace aidl::vendor::lineage::xiaomitouch {
+
+::ndk::ScopedAStatus XiaomiTouch::setModeValue(int32_t mode, int32_t value) {
+    if (!DISABLE_DEBUG) LOG(INFO) << TAG << ": " << "setModeValue called with value = " << value;
+
+    file_fd fd(open(TOUCH_DEV_PATH, O_RDWR));
+
+    int buf[3] = {TOUCH_ID, mode, value};
+
+    if (fd.get() == -1) {
+        if (!DISABLE_DEBUG) LOG(ERROR) << TAG << ": " << "Failed to open: " << TOUCH_DEV_PATH;
+    } else {
+        if (ioctl(fd.get(), TOUCH_IOC_SET_CUR_VALUE, &buf) == -1) {
+            if (!DISABLE_DEBUG) LOG(ERROR) << TAG << ": " << "Failed to write to: " << TOUCH_DEV_PATH;
+        } else {
+            if (!DISABLE_DEBUG) LOG(INFO) << TAG << ": " << "Wrote touch mode value as: " << value
+                << " for mode: " << mode;
+        }
+    }
+
+    return ::ndk::ScopedAStatus::ok();
+}
+
+XiaomiTouch::XiaomiTouch(void) {
+    if (!DISABLE_DEBUG) LOG(INFO) << TAG << ": " << "XiaomiTouch: Constructor";
+}
+
+}

touch/default/XiaomiTouch.h
@@ -1,0 +1,22 @@
+//
+// Copyright (C) 2023 Paranoid Android
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#pragma once
+
+#include <aidl/vendor/lineage/xiaomitouch/BnXiaomiTouch.h>
+
+#include <mutex>
+#include <thread>
+
+namespace aidl::vendor::lineage::xiaomitouch {
+
+class XiaomiTouch : public BnXiaomiTouch {
+    public:
+    XiaomiTouch(void);
+    ::ndk::ScopedAStatus setModeValue(int mode, int value) override;
+};
+
+}

touch/default/service.cpp
@@ -1,0 +1,33 @@
+//
+// Copyright (C) 2023 Paranoid Android
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include <android-base/logging.h>
+#include <android/binder_manager.h>
+#include <android/binder_process.h>
+#include <binder/ProcessState.h>
+#include <binder/IServiceManager.h>
+
+#include "XiaomiTouch.h"
+
+using ::aidl::vendor::lineage::xiaomitouch::XiaomiTouch;
+
+template <class C>
+static void registerAsService(std::shared_ptr<C> service, const char *inst) {
+  const std::string instance = std::string() + C::descriptor + "/" + inst;
+  binder_status_t status =
+      AServiceManager_addService(service->asBinder().get(), instance.c_str());
+  CHECK(status == STATUS_OK);
+  if (!DISABLE_DEBUG) LOG(INFO) << "Register done for instance " << inst;
+}
+
+int main() {
+  ABinderProcess_setThreadPoolMaxThreadCount(8);
+  if (!DISABLE_DEBUG) LOG(INFO) << "Starting XiaomiTouch service";
+  registerAsService(ndk::SharedRefBase::make<XiaomiTouch>(), "default");
+
+  ABinderProcess_joinThreadPool();
+  return -1; // should never get here
+}