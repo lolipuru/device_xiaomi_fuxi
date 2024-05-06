cc9a7f8b56721deccb57ee78733a2c034dd83849
Nolen Johnson <johnsonnolen@gmail.com>
2024-05-06 12:47:44 -0400

    fuxi: Actually include firmware
    Change-Id: I07ae71ac2ecd565a157a24a2596cb2d695fc11f0

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -279,3 +279,6 @@ WIFI_DRIVER_STATE_ON := "ON"
 WIFI_HIDL_FEATURE_DUAL_INTERFACE := true
 WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
 WPA_SUPPLICANT_VERSION := VER_0_8_X
+
+# Inherit from the proprietary version
+include vendor/xiaomi/fuxi/BoardConfigVendor.mk
