dff279547f9384ba0af9feab55fdf043a27d2924
Bruno Martins <bgcngm@gmail.com>
2025-04-24 21:36:24 +0100

    fuxi: Switch to PowerShare AIDL
    Change-Id: I97400638493a3b9d5a3b87fa6831c9affc7c3def

--- device.mk
+++ device.mk
@@ -274,7 +274,7 @@ PRODUCT_PACKAGES += \
 
 # PowerShare
 PRODUCT_PACKAGES += \
-    vendor.lineage.powershare@1.0-service.default
+    vendor.lineage.powershare-service.default
 
 # PSI
 PRODUCT_PACKAGES += \
